3f4725542284aaeefd66aa9ff55e9d6223a9fab3
Le Mike <mehdi.mikou@student.ecp.fr>
2020-05-28 19:26:38 +0200

    160 iterations par seconde

--- simulation.py
+++ simulation.py
@@ -8,7 +8,6 @@ from numba import njit
 def simulation_numba(urb_fin_new,ini_urb,order_urbanization,dist,index_exclu_1d_arr,index_uni,index_double):
     new_urb = np.zeros(len(order_urbanization),dtype=np.int64)
     lig,col = urb_fin_new.shape
-    i = 0
     for rand in order_urbanization:
         urb_fin_new_1d = urb_fin_new.reshape(lig*col).copy()
         growth_cells = np.where(urb_fin_new_1d==rand)[0]
@@ -28,15 +27,14 @@ def simulation_numba(urb_fin_new,ini_urb,order_urbanization,dist,index_exclu_1d_
         elif rand==2:
             new_val =  np.array(list(map(lambda x: 1 if x not in [0,255] else x , adj_cells))).reshape(2*dist+1,2*dist+1)
             urb_fin_new[x-dist:x+dist+1,y-dist:y+dist+1] = new_val
-        
+
             possib = [col*a + b  for a in range(x-dist,x+dist+1) for b in range(y-dist,y+dist+1) if (a,b)!=(x,y)]
-            urban_ini_list = [a for a in ini_urb]
-            
-            old_index = [a for a in possib if a in urban_ini_list][0]
-            x,y = index_double[old_index]
-            adj_cells_old = urb_fin_new[x-dist:x+dist+1,y-dist:y+dist+1].copy().reshape((2*dist+1)**2)          
-            new_val =  np.array(list(map(lambda x: 1 if x not in [0,255] else x , adj_cells_old))).reshape(2*dist+1,2*dist+1)
-            urb_fin_new[x-dist:x+dist+1,y-dist:y+dist+1] = new_val
+            for val in possib:
+                if len(np.where(ini_urb==possib[0]))>0:
+                    x,y = index_double[val]
+                    adj_cells_old = urb_fin_new[x-dist:x+dist+1,y-dist:y+dist+1].copy().reshape((2*dist+1)**2)          
+                    new_val =  np.array(list(map(lambda x: 1 if x not in [0,255] else x , adj_cells_old))).reshape(2*dist+1,2*dist+1)
+                    urb_fin_new[x-dist:x+dist+1,y-dist:y+dist+1] = new_val
 
         elif rand==3:
             if np.sum(adj_cells)==255:
@@ -52,6 +50,5 @@ def simulation_numba(urb_fin_new,ini_urb,order_urbanization,dist,index_exclu_1d_
         urb_fin_new_bis = urb_fin_new.copy().reshape(lig*col)
         urb_fin_new_bis[index_exclu_1d_arr] = 0
         urb_fin_new = urb_fin_new_bis.reshape((lig,col)).copy()
-        i = i+1
         
     return new_urb
